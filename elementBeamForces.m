2bfcda8694d9ff3aace68537bf3e4eb7725392fe
user.mail <mvanzulli@fing.edu.uy>
2021-03-11 14:20:09 -0300

    Version final entregada al tribunal

--- elementBeamForces.m
+++ elementBeamForces.m
@@ -1,21 +1,3 @@
-% Copyright (C) 2020, Jorge M. Perez Zerpa, J. Bruno Bazzano, Joaquin Viera, 
-%   Mauricio Vanzulli, Marcelo Forets, Jean-Marc Battini, Sebastian Toro  
-%
-% This file is part of ONSAS.
-%
-% ONSAS is free software: you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
-% the Free Software Foundation, either version 3 of the License, or
-% (at your option) any later version.
-%
-% ONSAS is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details.
-%
-% You should have received a copy of the GNU General Public License
-% along with ONSAS.  If not, see <https://www.gnu.org/licenses/>.
-
 function  [ fs, ks, stress, rotData ]= elementBeamForces( ...
   elemCoords, elemCrossSecParams, elemConstitutiveParams, solutionMethod, Ue, Udote, Udotdote, elemrho ) ;
 
@@ -32,7 +14,7 @@ G   = E/(2*(1+nu)) ;
 % -------------------------------
 
 % --- cross section ---
-if elemCrossSecParams(1) == 1 
+if elemCrossSecParams(1) == 1
     Area = elemCrossSecParams( 2 ) ;
     J    = elemCrossSecParams( 3 ) ;
     Iyy  = elemCrossSecParams( 4 ) ;
@@ -86,7 +68,7 @@ tg2 = dg (10:12);
 Rg1 = expon( tg1 ) ;
 Rg2 = expon( tg2 ) ;
 
-x21 = xs(4:6) - xs(1:3) ; 
+x21 = xs(4:6) - xs(1:3) ;
 d21 = dg(7:9) - dg(1:3) ;
 
 lo = sqrt( ( x21       )' * ( x21       ) ) ; %
@@ -104,22 +86,22 @@ q1 = Rg1 * Ro * [0 1 0]' ;
 q2 = Rg2 * Ro * [0 1 0]' ;
 q  = ( q1 + q2 ) / 2     ;
 
-%  ---  deformed z local axis  --- 
+%  ---  deformed z local axis  ---
 e3 = cross (e1, q) ;
 e3 = e3 / norm(e3) ;
 
-%  ---  deformed y local axis  --- 
+%  ---  deformed y local axis  ---
 e2 = cross (e3, e1);
 
-%  --- rotation matrix  --- 
+%  --- rotation matrix  ---
 Rr = [ e1 e2 e3 ] ;
 
 % --- local displacements ---
 
-%  ---  axial displacement  --- 
+%  ---  axial displacement  ---
 u  = l - lo;
 
-%  ---  local rotations  --- 
+%  ---  local rotations  ---
 Re1 = Rr' * Rg1 * Ro;
 Re2 = Rr' * Rg2 * Ro;
 
@@ -132,7 +114,7 @@ locDisp = [ u tl1' tl2' ] ;
 [fl, kl, strain, stress] = beamLocalStaticForces (u, tl1, tl2, lo, E, G, Area, Iyy, Izz, J ) ;
 
 %% transformation to the new local coordinates
-q  = Rr' *  q ;  
+q  = Rr' *  q ;
 q1 = Rr' * q1 ;
 
 nu = q(1)/q(2);
@@ -146,7 +128,7 @@ De2 = invTs( tl2 ) ;
 
 % --- matrix for transformation between global and relative ---
 % --- rotations/moments ---
-H  = [  1   O1   O1 ; ... 
+H  = [  1   O1   O1 ; ...
        O1' De1   O3 ; ...
        O1'  O3  De2 ] ;
 
@@ -203,7 +185,7 @@ EE=[Rr O3 O3 O3
 
 nab=[0
     (nu*(fe(2)+fe(5))+fe(3)+fe(6))/l
-    (fe(4)+fe(7))/l]; 
+    (fe(4)+fe(7))/l];
 
 Kg = B' * ke * B + Dr * fe(1) - EE*sF*G'*EE' + EE*G*nab*r' ;
 
@@ -251,111 +233,111 @@ if solutionMethod > 2
   % --- linear ---
   N1 = @(x) 1 -x/lo              ;
   N2 = @(x) x/lo				 ;
-  
+
   % --- cubic ---
   N3 = @(x) x*(1-x/lo)^2		 ;
-  N4 = @(x) -(1-x/lo)*(x^2)/lo   ; 
+  N4 = @(x) -(1-x/lo)*(x^2)/lo   ;
   N5 = @(x) (1-3*x/lo)*(1-x/lo)  ;
   N6 = @(x) (3*x/lo-2)*(x/lo)	 ;
-  
+
   N7 = @(x) N3(x)+N4(x)          ;
   N8 = @(x) N5(x)+N6(x)-1		 ;
   % -------------------------------------
-  
+
   P1    = @(x) [ 0      0       0  0      0      0 ; ...
                  0      0   N3(x)  0      0  N4(x) ; ...
-                 0 -N3(x)	      0  0 -N4(x)      0 ] ; 
-  
+                 0 -N3(x)	      0  0 -N4(x)      0 ] ;
+
   ul    = @(x) P1(x) * [ tl1; tl2 ] ; % Eq. 38
 
   P2    = @(x) [ N1(x)      0      0  N2(x)      0     0 ; ...
                      0  N5(x)      0      0  N6(x)     0 ; ...
-                     0      0  N5(x)      0      0 N6(x) ] ; 
-       
+                     0      0  N5(x)      0      0 N6(x) ] ;
+
   N     = @(x) [ N1(x)*I3   O3   N2(x)*I3    O3 ];
-  
-  H1    = @(x) N(x) + P1( x ) * P - 1*skew( ul(x) ) * G' ; 
-  
+
+  H1    = @(x) N(x) + P1( x ) * P - 1*skew( ul(x) ) * G' ;
+
   wdoter= G' * EE' * ddotg ;% Eq. 65
-  
+
   A1    = [   O1          O1   O1       O1 ;
                0 -1    0  O1 	 0  1  0	O1 ;
-               0  0 	-1  O1	 0  0  1  O1 ] ; 
-      
-  udotl = @(x)  P1(x) * P * EE' * ddotg ; 
-  
-  H1dot = @(x)  N7(x)/(l^2)*A1*(r' * ddotg) - skew( udotl(x) ) * G' ; 
-   
+               0  0 	-1  O1	 0  0  1  O1 ] ;
+
+  udotl = @(x)  P1(x) * P * EE' * ddotg ;
+
+  H1dot = @(x)  N7(x)/(l^2)*A1*(r' * ddotg) - skew( udotl(x) ) * G' ;
+
   ET = [skew(wdoter)      O3         		O3 			 O3			;
-        O3		skew(wdoter)  		O3   		 O3			;		
-        O3			O3 			skew(wdoter)     O3			;							
+        O3		skew(wdoter)  		O3   		 O3			;
+        O3			O3 			skew(wdoter)     O3			;
         O3			O3  			O3       skew(wdoter)   ];
-  
-  C1 = @(x)  skew(wdoter)*H1(x) + H1dot(x) -H1(x)*ET; 
-  
-  udot    = @(x) Rr*H1(x)*EE'*ddotg; 
-  udotdot = @(x) Rr*H1(x)*EE'*ddotdotg+Rr*C1(x)*EE'*ddotg; 
-  
+
+  C1 = @(x)  skew(wdoter)*H1(x) + H1dot(x) -H1(x)*ET;
+
+  udot    = @(x) Rr*H1(x)*EE'*ddotg;
+  udotdot = @(x) Rr*H1(x)*EE'*ddotdotg+Rr*C1(x)*EE'*ddotg;
+
   %% --- Matrix to compute wdot y wdtotdot ---
-  
-  H2 = @(x) P2(x)*P+G'; 
-  
+
+  H2 = @(x) P2(x)*P+G';
+
   wdot  = @(x) Rr*H2(x)*EE'*ddotg;
-  
-  
+
+
   A2    = [   O1    O1    O1     O1;
             0 0  1  O1  0 0 -1   O1;
             0 -1 0  O1  0 1  0   O1 ] ;
-        
+
   H2dot    = @(x)	N8(x)/l^2*A2*(r'*ddotg) ;
-  
+
   C2       = @(x) skew(wdoter)*H2(x) + H2dot(x) - H2(x)*ET ;
-  
+
   wdotdot  = @(x) Rr*H2(x)*EE'*ddotdotg  + Rr*C2(x)*EE'*ddotg ;
-  
+
   %% --- Tensor dyadc of Intertia ---
   %compute Rg(x)
   thethaRoof  = @(x) P2(x)*[tl1;tl2] ;
-  Rex         = @(x) expon(thethaRoof(x)) ; 
-  Rgx  	      = @(x) Rr*Rex(x)*Ro';   
-  
-  Irho		  = @(x) Rgx(x)*Ro*(Jrho)*(Rgx(x)*Ro)'; 
-  Irhoe       = @(x) Rr'*Irho(x)*Rr;   	 		
-  
+  Rex         = @(x) expon(thethaRoof(x)) ;
+  Rgx  	      = @(x) Rr*Rex(x)*Ro';
+
+  Irho		  = @(x) Rgx(x)*Ro*(Jrho)*(Rgx(x)*Ro)';
+  Irhoe       = @(x) Rr'*Irho(x)*Rr;
+
   % --- Compute interial force by quadrature ---
   xIntPoints = [ -sqrt(3/5)     0  sqrt(3/5)  ] ;
   wIntPoints = [        5/9	  8/9        5/9  ] ;
-  
+
   IntegrandoForce  = @(x)  H1(x)'*Rr'*Area*rho*udotdot(x) ...
                          + H2(x)'*Rr'*( ...
                            Irho(x)*wdotdot(x)...
                            + skew(wdot(x)) * Irho(x) * wdot(x) ...
-                         ) ;  
+                         ) ;
 
- IntegrandoMassMatrix  = @(x) 1*H1(x)'*Area*rho*H1(x)+1*H2(x)'*Irhoe(x)*H2(x); 
-  
+ IntegrandoMassMatrix  = @(x) 1*H1(x)'*Area*rho*H1(x)+1*H2(x)'*Irhoe(x)*H2(x);
+
   %% --- Compute C3 and C4 ---
-  
-  h1 = @(x) H1(x) * ddotg ; 
+
+  h1 = @(x) H1(x) * ddotg ;
   h2 = @(x) H2(x) * ddotg ;
-  
+
   rElem = [ [-1 0 0]   O1  [1 0 0] O1];
-  
-  F1    = [skew(udot(0))' skew(wdot(0))' skew(udot(lo))' skew(wdot(lo))']'; 
-  
-  C3  = @(x) -skew(h1(x))*G'  + (N7(x)/l^2)*A1*(ddotg*rElem)...	
-                +skew(wdoter)*P1(x)*P + H1(x)*F1*G'; 
-  
-  C4  = @(x) -skew(h2(x))*G' + (N8(x)/l^2)*A2*ddotg*rElem + H2(x)*F1*G'; 
-  
-  %% --- Compute Gyroscopic Matrix--- 
+
+  F1    = [skew(udot(0))' skew(wdot(0))' skew(udot(lo))' skew(wdot(lo))']';
+
+  C3  = @(x) -skew(h1(x))*G'  + (N7(x)/l^2)*A1*(ddotg*rElem)...
+                +skew(wdoter)*P1(x)*P + H1(x)*F1*G';
+
+  C4  = @(x) -skew(h2(x))*G' + (N8(x)/l^2)*A2*ddotg*rElem + H2(x)*F1*G';
+
+  %% --- Compute Gyroscopic Matrix---
   IntegrandoGyroMatrix  = @(x)  H2(x)' * ( ( skew(wdoter) * Irhoe(x) ) - skew( Irhoe(x) * wdoter) ) * H2(x) ...
-                              + H1(x)' * Area*rho*(C1(x) + C3(x))  + H2(x)'*Irhoe(x)*(C2(x)+C4(x)) ; 
-  
+                              + H1(x)' * Area*rho*(C1(x) + C3(x))  + H2(x)'*Irhoe(x)*(C2(x)+C4(x)) ;
+
   sumForce = zeros (12, 1 ) ;
   sumGyro  = zeros (12    ) ;
   sumMass  = zeros (12    ) ;
-  
+
 
   for ind = 1 : length( xIntPoints )
     sumForce = sumForce ...
@@ -371,9 +353,9 @@ if solutionMethod > 2
   Fine       = EE * sumForce      ;
   GyroMatrix = EE * sumGyro * EE' ;
   MassMatrix = EE * sumMass * EE' ;
-    
+
   %% --- Add Bt Matrix ---
-  
+
   Bt=[I3   O3       O3      O3
       O3 inv(Dg1)'    O3      O3
       O3     O3      I3      O3
@@ -381,15 +363,12 @@ if solutionMethod > 2
   MassMatrix =MassMatrix*Bt ;
   GyroMatrix =GyroMatrix*Bt ;
   %% --- Switch base ---
-  Fine       = Cambio_Base(Fine); 
-  GyroMatrix = Cambio_Base(GyroMatrix); 
-  MassMatrix = Cambio_Base(MassMatrix); 
+  Fine       = Cambio_Base(Fine);
+  GyroMatrix = Cambio_Base(GyroMatrix);
+  MassMatrix = Cambio_Base(MassMatrix);
 
   fs{3} = Fine ;
   ks{2} = GyroMatrix ;
   ks{3} = MassMatrix ;
 
 end
-
-
-
